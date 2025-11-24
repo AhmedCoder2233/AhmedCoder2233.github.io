0bdec265aee7d72d1c5574324262702ebe9714cd
Ahmed Memon <ahmedpubgking3388@gmail.com>
2025-11-24 18:05:13 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,12 +23,12 @@ const config = {
   url: 'https://ahmedcoder2233.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Testing',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'AhmedCoder2233', // Usually your GitHub org/user name.
+  projectName: 'AhmedCoder2233.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
